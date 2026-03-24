2a53cc4978032d349125b2c673dbc59838ff966e
BronsonPro <nidhidesai89@yahoo.com>
2026-03-24 15:13:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -554,6 +554,8 @@ master_df = None
 if master_file:
     master_df = pd.read_excel(master_file)
     master_df.columns = master_df.columns.astype(str).str.strip()
+    master_df = normalize(master_df, is_master=True)  # ← ADD THIS LINE
+
 
 
 # ================== STEP 3 ==================
