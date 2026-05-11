8c31546a812109ba7cccbe6d29b7c685e2f96408
BronsonPro <nidhidesai89@yahoo.com>
2026-05-11 16:03:43 +0530

    Update Foy.py

--- parsers/Foy.py
+++ parsers/Foy.py
@@ -149,7 +149,7 @@ def _extract_all(pdf_path: str):
             'Product Name':  name,
             'HSN Code':      m.group(4),
             'Qty':           int(m.group(5)),
-            'Base Rate':     float(m.group(6))
+            'Base Rate':     float(m.group(6)),
             'GST %':         gst_pct,
             'Discount':      float(m.group(7)),
             'Taxable Value': float(m.group(8)),
